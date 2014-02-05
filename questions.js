fa67789c17e053f98183dc47689a78f324c23787
inghramrb <inghramrb@upmc.edu>
2014-02-05 17:53:35 -0500

    Completed more ASR

--- questions.js
+++ questions.js
@@ -1,116 +1,113 @@
-
-
-    var questioneers =  [ 
-     {
-        name: 'Adult Self Report',
-        sections: [
-	        {
-	           name: 'ID',
-	           type: 'normal',
-	           questions: [
-	             { 
-	              type: "radio",
-	              label: 'Your Gender',
-	              options: ['Male','Female']},
-	             {
-	              label: 'Age',
-	              type:'text',
-	             },
-	             {	            
-	             	label: 'Birthdate mm/dd/yyyy',
-	             	type: 'text',
-	             	//to do: the date needs to be formatted into a series of dropdown menus
-	             },
-	           ]
-	        },
-	        {
-	           name: 'Education and Work',
-	           type: 'normal',
-	           questions: [
-	             { 
-	              type: "radio",
-	              label: 'Highest Level Of Education',
-	              options: ['No highschool diploma and no GED',
-	                     'GED', 
-	                     'High school graduate',
-	                     'Some college but no college degree',
-	                     "Associate's Degree",
-	                     "Bachelor's or RN Degree",
-	                     'Some graduate school but no graduate degree',
-	                     "Master's Degree",
-	                     'Docoral or Law Degree', 
-	                     'Other education (sepcify)', //to do: put a free text field so people can specify their 'other education'
-	                       ]
-	             },
-	             {
-	             	label: 'Your Usual Types of Work',
-	             	type:'text',
-	             	desc:  'Please be specific',
-	             },
-	             {
-	             	label: "Your Spouse or Partner's Usual Types of Work",
-	             	type: 'text',
-	             	desc: 'Please be specific',
-	             },
-	           ]
-        	},
-        	{
-        		name: 'Friends',
-        		type: 'normal',
-        		questions: [
-        		 {
-        		 	type: 'radio',
-        		 	label: 'About how many close friends do you have? (Do not include family members.)',
+var ASR = {
+  name: 'Adult Self Report',
+      sections: [
+          {
+             name: 'ID',
+             type: 'normal',
+             questions: [
+               { 
+                type: "radio",
+                label: 'Your Gender',
+                options: ['Male', 'Female']
+               },
+               {
+                label: 'Age',
+                type:'text',
+               },
+               {              
+                label: 'Birthdate mm/dd/yyyy',
+                type: 'text',
+                //to do: the date needs to be formatted into a series of dropdown menus
+               },
+             ]
+          },   
+          {
+             name: 'Education and Work',
+             type: 'normal',
+             questions: [
+               { 
+                type: "radio",
+                label: 'Highest Level Of Education',
+                options: ['No highschool diploma and no GED',
+                       'GED', 
+                       'High school graduate',
+                       'Some college but no college degree',
+                       "Associate's Degree",
+                       "Bachelor's or RN Degree",
+                       'Some graduate school but no graduate degree',
+                       "Master's Degree",
+                       'Docoral or Law Degree', 
+                       'Other education (sepcify)', //to do: put a free text field so people can specify their 'other education'
+                         ]
+               },
+               {
+                label: 'Your Usual Types of Work',
+                type:'text',
+                desc:  'Please be specific',
+               },
+               {
+                label: "Your Spouse or Partner's Usual Types of Work",
+                type: 'text',
+                desc: 'Please be specific',
+               },
+             ]
+          },
+          {
+            name: 'Friends',
+            type: 'normal',
+            questions: [
+             {
+              type: 'radio',
+              label: 'About how many close friends do you have? (Do not include family members.)',
                     options: ['None', 
-                    		'1',
-                    		'2 or 3',
-                    		'4 or more',
-                    		]
+                        '1',
+                        '2 or 3',
+                        '4 or more',
+                        ]
                   },
-                  { 		
-                	type: 'radio',
-                	label: 'About how many times a month do you have contact with any of your close friends? (Include in-person contacts, phone, letters, e-mail.)',
-                	options: ['Less than 1',
-                			'1 or 2',
-                			'3 or 4',
-                			'5 or more',
-                			]
-                },
-                {
+                  {     
+                  type: 'radio',
+                  label: 'About how many times a month do you have contact with any of your close friends? (Include in-person contacts, phone, letters, e-mail.)',
+                  options: ['Less than 1',
+                      '1 or 2',
+                      '3 or 4',
+                      '5 or more',
+                      ]
+                  },
+                  {
                     type: 'radio',
                     label: 'How well do you get along with your close friends?',
                     options: ["Not as well as I'd like",
-                    		'Average',
-                    		'Above average',
-                    		'Far above average',
-                    		]
-               	},
-               	{
-               		type: 'radio',
-               		label: 'About how many times a month do any friends ro family visit you?',
-               		options: ['Less than 1',
-               				'1 or 2',
-               				'3 or 4',
-               				'5 or more',
-               				]
-
-                }], 
-           	},
-           	{
-           		name: 'Spouse or Partner',
-           		type: 'normal',
-           		questions: [
-           		    {
-           		    type: 'radio',
-           		    label: 'What is your marital status?',
-           		    options: ['Never been married',
-           		    'Married, living with spouse',
-           		    'Widowed',
-           		    'Married but separated from spouse',
-           		    'Divorced',
-           		    'Other (please specify)'], //to do: make open text field so subjects can specify other
-           		    },
-           		    {
+                        'Average',
+                        'Above average',
+                        'Far above average',
+                        ]
+                  },
+                  {
+                  type: 'radio',
+                  label: 'About how many times a month do any friends ro family visit you?',
+                  options: ['Less than 1',
+                      '1 or 2',
+                      '3 or 4',
+                      '5 or more',
+                      ]
+                  }], 
+            },
+            {
+              name: 'Spouse or Partner',
+              type: 'normal',
+              questions: [
+                  {
+                  type: 'radio',
+                  label: 'What is your marital status?',
+                  options: ['Never been married',
+                  'Married, living with spouse',
+                  'Widowed',
+                  'Married but separated from spouse',
+                  'Divorced',
+                  'Other (please specify)'], //to do: make open text field so subjects can specify other
+                  },
+                  {
                   type: 'radio',
                   label: 'At any time in the past 6 months, did you live with your spouse or with a partner?',
                   options: ['No - please skip to "Family" section.',
@@ -134,25 +131,24 @@
                 { type: 'radio', label: "My spouse or partner's behavior annoys me"},
                 ]      
             },
-
-     		 {
-	           name: 'Family',
-	           type: 'grid',
-	           labels: ['Not Applicable','Worse than Average','variable or Average','Better than Average','No Contact'],
-	           desc: "Compared with others, how well do you:",
-	           questions: [
-	             { type: "radio", label: 'A. Get along with your brother(s)'},
-	             { type: "radio", label: 'B. Get along with your sister(s)'},
-	             { type: "radio", label: 'C. Get along with your mother' },
-	             { type: "radio", label: 'D. Get along with your father' },
-               { type: "radio", label: 'E.1 Get along with your Oldest child'},
-               { type: "radio", label: 'E.2 Get along with your 2nd oldest child'},
-               { type: "radio", label: 'E.3 Get along with your 3rd oldest child'},
-               { type: "radio", label: 'E.4 Get along Other children'},
-               { type: "radio", label: 'F. Get along with your stepchildren'},
+            {
+              name: 'Family',
+              type: 'grid',
+              labels: ['Not Applicable','Worse than Average','variable or Average','Better than Average','No Contact'],
+              desc: "Compared with others, how well do you:",
+              questions: [
+                { type: "radio", label: 'A. Get along with your brother(s)'},
+                { type: "radio", label: 'B. Get along with your sister(s)'},
+                { type: "radio", label: 'C. Get along with your mother' },
+                { type: "radio", label: 'D. Get along with your father' },
+                { type: "radio", label: 'E.1 Get along with your Oldest child'},
+                { type: "radio", label: 'E.2 Get along with your 2nd oldest child'},
+                { type: "radio", label: 'E.3 Get along with your 3rd oldest child'},
+                { type: "radio", label: 'E.4 Get along Other children'},
+                { type: "radio", label: 'F. Get along with your stepchildren'},
               ]
-          },
-          {
+           },
+           {
             name: 'Job',
             type: 'normal',
             questions: [
@@ -184,57 +180,107 @@
                { type: "radio", label: 'I. I worry too much about work'},
                ]
            },
-           //MAKE A FIELD BELOW FOR THEM TO DESCRIBE THEIR JOB 
-]},
-          
-// End ASR Questionnaire
+           {
+            name: 'Education',
+            type: 'normal',
+            questions: [
+            {
+              type: 'radio',
+              labels: "At any time in the past 6 months, did you attend school, college, or any other educational or training program?",
+              options: ['No--Please skip to the next section',
+              'Yes--What kind of school or program?'],
+            },
+            {
+              type: 'text',
+              label: 'Education Description',
+            }],
+          },
+          {
+            name: 'Education Experience',
+            type: 'grid',
+            labels: ['Not True', 'Somewhat or Sometimes True', 'Very True or Often True'],
+            desc: "Choose option 0, 1, or 2 for items A-E to describe your educational experience during the past 6 months",
+            questions: [
+            { type: "radio", label: 'A. I get along well with other students'},
+            { type: "radio", label: 'B. I achieve what I am capable of'},
+            { type: "radio", label: 'C. I have trouble finishing assignments'},
+            { type: "radio", label: 'D. I am satisfied with my educational situation'},
+            { type: "radio", label: 'E. I do thingst hat may cause me to fail'},
+            ],
+          },
+          {
+            name: 'Other Questions', 
+            type: 'normal',
+            questions: [
+            {
+              type: 'radio',
+              labels: "Do you have any illness, disability, or handicap?",
+              options: [
+              'No', 
+              'Yes--please describe'],
+            },
+            {
+              type: 'text',
+              label: 'If "yes" describe below',
+            },
+            {
+              type: 'text',
+              label: 'Please describe your concerns or worries about family, work, education'
+            }],
+          }
+   ]        
+};
 
-      {
-      	name: 'Reward Ranking',
-      	sections: [
-      	   	{
-	           name: 'Reward',
-	           type: 'rank',
-	           desc: "rank what you like 1(best) to 13 (worst):",
-	           questions: [
-	             { label: "Applebee's"},
-	             { label: "Domino's Pizza"},
-	             { label: "Starbucks"},
-	             { label: "American Eagle" },
-	           ]
-        	}
+    var RewardRanking = {
+        name: 'Reward Ranking',
+        sections: [
+            {
+             name: 'Reward',
+             type: 'rank',
+             desc: "rank what you like 1(best) to 13 (worst):",
+             questions: [
+               { label: "Applebee's"},
+               { label: "Domino's Pizza"},
+               { label: "Starbucks"},
+               { label: "American Eagle" },
+             ]
+          }
         ]
-      },
-// End Reward Ranking Questionnaire
-      {
-      	name: 'Demographic',
-      	sections: [
-      	   	{
-	           name: 'Background',
-	           type: 'normal',
-	           questions: [
-	             { 
-	              type: "radio",
-	              label: 'Do you Consider yourself to be Hispanic or Latina?',
-	              desc: '<b>Hispanic or Latino</b>: A person of Mexican, Puerto Rican, Cuban, South or Central American, ...',
-	              options: ['Hispanic or Latino','Not Hispanic or Latino']
-	             },
-	             {
-	             	label: 'What race do you consider yourself',
-	             	desc: 'Select one or more of the following',
-	             	type:'checkbox',
-	             	/* array of objects because we need to set value indepenently for each option/label */
-	             	options: [{value:0, label:'American Indian'},
-	             	          {value:0, label:'Asian'},
-	             	          {value:0, label:'Black or African American'},
-	             	          {value:0, label:'Native Hawaiian or Other Pacific Islander'},
-	             	          {value:0, label:'White'},
-	             	          {value:0, label:'Do not wish to provide'}]
-	             	
-	             }
-	           ]
-        	}
+      };
+
+    var Demographic = {
+        sections: [
+            {
+             name: 'Background',
+             type: 'normal',
+             questions: [
+               { 
+                type: "radio",
+                label: 'Do you Consider yourself to be Hispanic or Latina?',
+                desc: '<b>Hispanic or Latino</b>: A person of Mexican, Puerto Rican, Cuban, South or Central American, ...',
+                options: ['Hispanic or Latino','Not Hispanic or Latino']
+               },
+               {
+                label: 'What race do you consider yourself',
+                desc: 'Select one or more of the following',
+                type:'checkbox',
+                /* array of objects because we need to set value indepenently for each option/label */
+                options: [{value:0, label:'American Indian'},
+                          {value:0, label:'Asian'},
+                          {value:0, label:'Black or African American'},
+                          {value:0, label:'Native Hawaiian or Other Pacific Islander'},
+                          {value:0, label:'White'},
+                          {value:0, label:'Do not wish to provide'}]
+                
+               }
+             ]
+          }
         ]
-      }
-    ];
-//End all questionnaires
+      };
+  
+    var questioneers = [
+    ASR,
+    RewardRanking,
+    Demographic
+    ]
+  
